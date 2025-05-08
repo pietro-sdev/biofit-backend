1d0d663a08a725253d9f10d6caf997edca45abf6
Pietro dos Santos Menezes <pietro.dsantos77@gmail.com>
2025-05-08 14:36:55 -0300

    fix/ajustado get produtos

--- src/main/java/com/br/biofit/Model/ImagemProduto.java
+++ src/main/java/com/br/biofit/Model/ImagemProduto.java
@@ -1,5 +1,6 @@
 package com.br.biofit.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -30,5 +31,6 @@ public class ImagemProduto {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "produto_id")
+    @JsonIgnore
     private Produto produto;
 }
